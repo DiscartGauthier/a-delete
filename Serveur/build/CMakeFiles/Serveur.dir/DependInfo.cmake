111e51a0783f80d5ae4bdbac6de349d3320c5cd5
DiscartGauthier <gauthier.discart@student.hepl.be>
2024-10-13 05:28:02 +0200

    test

---
+++ Serveur/build/CMakeFiles/Serveur.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/student/Bureau/RTI/Me/AppliBooker/ClientBookEncoderQt/src/OBEP.cpp" "/home/student/Bureau/RTI/Me/AppliBooker/Serveur/build/CMakeFiles/Serveur.dir/home/student/Bureau/RTI/Me/AppliBooker/ClientBookEncoderQt/src/OBEP.cpp.o"
+  "/home/student/Bureau/RTI/Me/AppliBooker/ClientBookEncoderQt/src/TCP.cpp" "/home/student/Bureau/RTI/Me/AppliBooker/Serveur/build/CMakeFiles/Serveur.dir/home/student/Bureau/RTI/Me/AppliBooker/ClientBookEncoderQt/src/TCP.cpp.o"
+  "/home/student/Bureau/RTI/Me/AppliBooker/Serveur/src/Serveur.cpp" "/home/student/Bureau/RTI/Me/AppliBooker/Serveur/build/CMakeFiles/Serveur.dir/src/Serveur.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../ClientBookEncoderQt/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Serveur/build/CMakeFiles/Serveur.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Serveur.dir/home/student/Bureau/RTI/Me/AppliBooker/ClientBookEncoderQt/src/OBEP.cpp.o"
+  "CMakeFiles/Serveur.dir/home/student/Bureau/RTI/Me/AppliBooker/ClientBookEncoderQt/src/TCP.cpp.o"
+  "CMakeFiles/Serveur.dir/src/Serveur.cpp.o"
+  "Serveur"
+  "Serveur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Serveur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
